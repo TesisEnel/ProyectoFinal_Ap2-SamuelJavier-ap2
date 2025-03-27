6aa8396443ea84054d2aeef3f7bf58cb524e72c9
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-03-27 13:21:34 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/MainActivity.kt
@@ -0,0 +1,39 @@
+package edu.ucne.proyectofinal_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.FirebaseApp
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.proyectofinal_ap2.presentation.navigation.AppNavHost
+import edu.ucne.proyectofinal_ap2.ui.theme.ProyectoFinal_Ap2Theme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoFinal_Ap2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        val navController = rememberNavController()
+                        AppNavHost(navController)
+                    }
+                }
+            }
+        }
+    }
+}
