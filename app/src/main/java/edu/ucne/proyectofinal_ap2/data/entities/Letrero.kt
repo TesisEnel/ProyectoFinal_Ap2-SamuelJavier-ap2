5744fb97bb477e521fc824f283b78c9ba4e91fb3
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-01 01:01:46 -0400

    Cambiando los campos del modelo letrero

--- app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Letrero.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Letrero.kt
@@ -1,7 +1,10 @@
 package edu.ucne.proyectofinal_ap2.data.entities
 
 data class Letrero(
-    val id: Int,
-    val nombre: String,
-    val imagen: Int
+
+val id: String = "",
+val nombre: String = "",
+val descripcion: String = "",
+val imagenUrl: String = ""
 )
+
