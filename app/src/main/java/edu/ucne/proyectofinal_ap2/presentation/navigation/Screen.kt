8940c3ac2fcd7964ba3e1e12cbdbc9abb8a848d4
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-01 01:05:48 -0400

    Agregando mas pantallas al Screen

--- app/src/main/java/edu/ucne/proyectofinal_ap2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/presentation/navigation/Screen.kt
@@ -1,5 +1,6 @@
 package edu.ucne.proyectofinal_ap2.presentation.navigation
 
+import android.net.Uri
 import kotlinx.serialization.Serializable
 
 sealed class Screen(val route: String) {
@@ -10,11 +11,27 @@ sealed class Screen(val route: String) {
     object MisPedidosScreen : Screen("MisPedidosScreen")
     object PerfilScreen : Screen("PerfilScreen")
     object PersonalizarPedidoScreen : Screen("PersonalizarPedidoScreen")
-    object MaterialesScreen : Screen("MaterialesScreen")
-    object DetalleLetreroScreen : Screen("DetalleLetreroScreen")
+    object ClienteMenuScreen : Screen("ClienteMenuScreen")
+    object DetalleLetreroScreen : Screen("detalleLetrero/{nombre}/{descripcion}/{imagenUrl}") {
+        fun createRoute(nombre: String, descripcion: String, imagenUrl: String): String {
+            return "detalleLetrero/${Uri.encode(nombre)}/${Uri.encode(descripcion)}/${Uri.encode(imagenUrl)}"
+        }
+    }
     object AdminMenuScreen : Screen("AdminMenuScreen")
+    object HomeClienteCatalogoScreen : Screen("HomeClienteCatalogoScreen")
     object AgregarLetreroScreen : Screen("AgregarLetreroScreen")
     object OpcionesLetreroScreen : Screen("OpcionesLetreroScreen")
     object EditarLetreroScreen : Screen("EditarLetreroScreen ")
     object EliminarLetreroScreen : Screen("EliminarLetreroScreen ")
+    object AgregarMaterialScreen : Screen("AgregarMaterialScreen")
+    object OpcionesMaterialesScreen : Screen("OpcionesMaterialesScreen ")
+    object EditarMaterialesScreen : Screen("EditarMaterialesScreen")
+    object EliminarMaterialesScreen : Screen("EliminarMaterialesScreen")
+    object CrearPedidoScreen : Screen("CrearPedidoScreen")
+    object  PagoInstruccionScreen : Screen(" PagoInstruccionScreen")
+    object  ListaPedidosAdminScreen: Screen("ListaPedidosAdminScreen")
+
+
+
+
 }
