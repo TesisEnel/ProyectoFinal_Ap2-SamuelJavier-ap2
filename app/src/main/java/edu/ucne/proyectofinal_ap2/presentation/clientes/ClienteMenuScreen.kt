65dd585bd22b22427e0d2864a999cfb28b25639a
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-01 00:56:16 -0400

    Creando y programando la pantalla para el menu del cliente

---
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/presentation/clientes/ClienteMenuScreen.kt
@@ -0,0 +1,87 @@
+package edu.ucne.proyectofinal_ap2.presentation.clientes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import edu.ucne.proyectofinal_ap2.R
+
+@Composable
+fun ClienteMenuScreen(
+    onHacerPedido: () -> Unit,
+    onMisPedidos: () -> Unit,
+    onPerfil: () -> Unit,
+    onCatalogo: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Bienvenido al Catálogo",
+            style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            item {
+                OpcionCard("Hacer Pedido", R.drawable.editar) { onHacerPedido() }
+            }
+
+            item {
+                OpcionCard("Mis Pedidos", R.drawable.pedido) { onMisPedidos() }
+            }
+
+            item {
+                OpcionCard("Mi Perfil", R.drawable.perfil) { onPerfil() }
+            }
+            item {
+                OpcionCard("Ver Catalogo", R.drawable.perfil) { onCatalogo() }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun OpcionCard(titulo: String, imagenRes: Int, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .clickable { onClick() },
+        elevation = 6.dp
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imagenRes),
+                contentDescription = titulo,
+                modifier = Modifier.size(100.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = titulo,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+    }
+}
