9d3face07a4b65e8cef85433beeb792a5cceea54
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-01 01:02:56 -0400

    Cambianod los atributos del modelo material

--- app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Material.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Material.kt
@@ -1,8 +1,8 @@
 package edu.ucne.proyectofinal_ap2.data.entities
 
+
 data class Material(
-    val id: Int,
-    val nombre: String,
-    val descripcion: String,
-    val imagen: Int
+    val nombre: String = "",
+    val descripcion: String = "",
+    val imagenUrl: String = ""
 )
