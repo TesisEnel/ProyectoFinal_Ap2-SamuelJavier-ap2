54b77fe31418bd4c7b7ff5f0dceb006dd3df5291
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-02 04:14:08 -0400

    Agregando el campo precioCm2 a material

--- app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Material.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Material.kt
@@ -4,5 +4,7 @@ package edu.ucne.proyectofinal_ap2.data.entities
 data class Material(
     val nombre: String = "",
     val descripcion: String = "",
-    val imagenUrl: String = ""
+    val imagenUrl: String = "",
+    val precioCm2: Double = 0.0
+
 )
