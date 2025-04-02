74a67863335b2ed01eade9c5f76879677b36dbf0
samueljavier880@gmail.com <samueljavier880@gmail.com>
2025-04-02 04:15:27 -0400

    Agregando las nuevas rutas y pantallas

--- app/src/main/java/edu/ucne/proyectofinal_ap2/presentation/navigation/NavigationHost.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/presentation/navigation/NavigationHost.kt
@@ -1,13 +1,10 @@
 package edu.ucne.proyectofinal_ap2.presentation.navigation
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 
 import android.widget.Toast
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -15,10 +12,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import edu.ucne.proyectofinal_ap2.data.entities.Material
-import edu.ucne.proyectofinal_ap2.presentation.login.AuthViewModel
+import com.google.firebase.auth.FirebaseAuth
+import edu.ucne.proyectofinal_ap2.presentation.viewModels.AuthViewModel
 import edu.ucne.proyectofinal_ap2.presentation.login.LoginScreen
 import edu.ucne.proyectofinal_ap2.presentation.login.RegisterScreen
 import edu.ucne.proyectofinal_ap2.presentation.menu.HomeScreen
@@ -27,7 +23,9 @@ import edu.ucne.proyectofinal_ap2.presentation.pedidos.VerPedidosScreen
 import edu.ucne.proyectofinal_ap2.presentation.perfil.PerfilScreen
 import edu.ucne.proyectofinal_ap2.presentation.admin.ListaPedidosAdminScreen
 import edu.ucne.proyectofinal_ap2.presentation.clientes.ClienteMenuScreen
-import edu.ucne.proyectofinal_ap2.presentation.menu.AdminMenuScreen
+import edu.ucne.proyectofinal_ap2.presentation.admin.AdminMenuScreen
+import edu.ucne.proyectofinal_ap2.presentation.admin.ListaUsuariosScreen
+import edu.ucne.proyectofinal_ap2.presentation.clientes.CuentaBancoScreen
 import edu.ucne.proyectofinal_ap2.presentation.menu.AgregarLetreroScreen
 import edu.ucne.proyectofinal_ap2.presentation.menu.AgregarMaterialScreen
 import edu.ucne.proyectofinal_ap2.presentation.menu.EditarLetreroScreen
@@ -44,6 +42,7 @@ import edu.ucne.proyectofinal_ap2.presentation.menu.OpcionesLetreroScreen
 import edu.ucne.proyectofinal_ap2.presentation.menu.OpcionesMaterialesScreen
 import edu.ucne.proyectofinal_ap2.presentation.pedidos.CrearPedidoScreen
 import edu.ucne.proyectofinal_ap2.presentation.viewModels.PedidoViewModel
+import edu.ucne.proyectofinal_ap2.presentation.viewModels.UsuarioViewModel
 
 
 @Composable
@@ -106,7 +105,9 @@ fun AppNavHost(navHostController: NavHostController) {
                 onOpcionesMateriales = { navHostController.navigate(Screen.OpcionesMaterialesScreen.route)},
                 onMisPedidos =  { navHostController.navigate(Screen.MisPedidosScreen.route)},
                 onHacerPedidos =  { navHostController.navigate(Screen.CrearPedidoScreen.route)},
-                onCatalogo = { navHostController.navigate(Screen.HomeClienteCatalogoScreen.route)}
+                onCatalogo = { navHostController.navigate(Screen.HomeClienteCatalogoScreen.route)},
+                onVerUsuariosClick = { navHostController.navigate(Screen.ListaUsuariosScreen.route)},
+                onCuentaBanco = { navHostController.navigate(Screen.CuentaBancoScreen.route)}
             )
         }
 
@@ -148,19 +149,27 @@ fun AppNavHost(navHostController: NavHostController) {
             AdminMenuScreen(
                 onVerPedidosClick = { navHostController.popBackStack() },
                 onOpcionesLetrero = { navHostController.navigate(Screen.OpcionesLetreroScreen.route)},
-                onOpcionesMateriales = { navHostController.navigate(Screen.OpcionesMaterialesScreen.route)}
-
+                onOpcionesMateriales = { navHostController.navigate(Screen.OpcionesMaterialesScreen.route)},
+                onVerUsuariosClick = { navHostController.navigate(Screen.ListaUsuariosScreen.route)},
+                onCerrarSesion = {
+                    FirebaseAuth.getInstance().signOut()
+                    navHostController.navigate("login") {
+                        popUpTo("admin") { inclusive = true }
+                    }
+                },
+                onPerfilClick =  { navHostController.navigate(Screen.PerfilScreen.route)}
             )
         }
 
+
         composable(Screen.EditarLetreroScreen.route) {
             EditarLetreroScreen(
-
+      navController= navHostController
             )
         }
         composable(Screen.EliminarLetreroScreen.route) {
             EliminarLetreroScreen(
-
+navController = navHostController
             )
         }
 
@@ -170,7 +179,8 @@ fun AppNavHost(navHostController: NavHostController) {
                 viewModel = LetreroViewModel,
                 onLetreroAgregado = {
                     navHostController.popBackStack()
-                }
+                },
+                navController = navHostController
             )
         }
 
@@ -178,7 +188,8 @@ fun AppNavHost(navHostController: NavHostController) {
             OpcionesLetreroScreen(
                 onEditarClick = { navHostController.navigate(Screen.EditarLetreroScreen.route)},
                 onAgregarClick = { navHostController.navigate(Screen.AgregarLetreroScreen.route)},
-                onEliminarClick = { navHostController.navigate(Screen.EliminarLetreroScreen.route)}
+                onEliminarClick = { navHostController.navigate(Screen.EliminarLetreroScreen.route)},
+                navController = navHostController
             )
         }
 
@@ -187,7 +198,8 @@ fun AppNavHost(navHostController: NavHostController) {
                 onEditarClick = { navHostController.navigate(Screen.EditarMaterialesScreen.route)},
                 onAgregarClick = { navHostController.navigate(Screen.AgregarMaterialScreen.route)},
 
-                onEliminarClick = { navHostController.navigate(Screen.EliminarMaterialesScreen.route)}
+                onEliminarClick = { navHostController.navigate(Screen.EliminarMaterialesScreen.route)},
+                navController = navHostController
             )
         }
 
@@ -208,7 +220,8 @@ fun AppNavHost(navHostController: NavHostController) {
                 onMaterialClick = { material ->
                 },
                 letreroViewModel = letreroViewModel,
-                materialViewModel = materialViewModel
+                materialViewModel = materialViewModel,
+                navController = navHostController
             )
         }
 
@@ -218,20 +231,31 @@ fun AppNavHost(navHostController: NavHostController) {
                 viewModel = MaterialViewModel,
                 onMaterialAgregado = {
                     navHostController.popBackStack()
-                }
+                },
+                navController = navHostController
             )
         }
         composable(Screen.EliminarMaterialesScreen.route) {
             EliminarMaterialScreen(
+                navController = navHostController
 
             )
         }
 
         composable(Screen.EditarMaterialesScreen.route) {
             EditarMaterialScreen(
-
+                  navController = navHostController
             )
         }
+
+        composable(Screen.ListaUsuariosScreen.route) {
+            val usuarioViewModel: UsuarioViewModel = hiltViewModel()
+            ListaUsuariosScreen(
+                navController = navHostController,
+                usuarioViewModel = usuarioViewModel
+            )
+        }
+
 
 
         composable(
@@ -277,10 +301,16 @@ fun AppNavHost(navHostController: NavHostController) {
             PagoInstruccionScreen(navController = navHostController)
         }
 
+        composable(Screen.CuentaBancoScreen.route) {
+            CuentaBancoScreen(navController = navHostController)
+        }
+
         composable(Screen.ListaPedidosAdminScreen.route) {
             val pedidoViewModel: PedidoViewModel = hiltViewModel()
             ListaPedidosAdminScreen(
-                pedidoViewModel = pedidoViewModel
+                pedidoViewModel = pedidoViewModel,
+                navHostController = navHostController
+
             )
         }
 
@@ -289,13 +319,15 @@ fun AppNavHost(navHostController: NavHostController) {
         composable(Screen.PerfilScreen.route) {
 
             PerfilScreen(
+                navController = navHostController
 
             )
         }
         composable(Screen.MisPedidosScreen.route) {
             val pedidoViewModel: PedidoViewModel = hiltViewModel()
             MisPedidosScreen(
-                pedidoViewModel = pedidoViewModel
+                pedidoViewModel = pedidoViewModel,
+                navController = navHostController
             )
         }
 
@@ -304,7 +336,15 @@ fun AppNavHost(navHostController: NavHostController) {
                 onPerfil = { navHostController.popBackStack() },
                 onMisPedidos = { navHostController.navigate(Screen.MisPedidosScreen.route)},
                 onHacerPedido = { navHostController.navigate(Screen.CrearPedidoScreen.route)},
-                onCatalogo = { navHostController.navigate(Screen.HomeClienteCatalogoScreen.route)}
+                onCatalogo = { navHostController.navigate(Screen.HomeClienteCatalogoScreen.route)},
+                onCerrarSesion = {
+                    FirebaseAuth.getInstance().signOut()
+                    navHostController.navigate("login") {
+                        popUpTo("cliente") { inclusive = true }
+                    }
+                },
+                onPerfilClick = { navHostController.navigate(Screen.PerfilScreen.route)},
+                onCuentaBanco = {navHostController.navigate(Screen.CuentaBancoScreen.route)}
             )
         }
 
