779dd305755a5e3bd174459aad43beee0380e1a3
SamyJp23 <samueljavier880@gmail.com>
2025-04-09 15:00:41 -0400

    Agregando los nuevos campos a la entidad pedido

--- app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Pedido.kt
+++ app/src/main/java/edu/ucne/proyectofinal_ap2/data/entities/Pedido.kt
@@ -3,6 +3,8 @@ package edu.ucne.proyectofinal_ap2.data.entities
 
 data class Pedido(
     val id: String = "",
+    val nombrePedido: String = "",
+    val descripcion: String = "",
     val material: String = "",
     val medida: String = "",
     val alto: String = "",
@@ -16,6 +18,11 @@ data class Pedido(
     val telefono: String = "",
     val direccion: String = "",
     val cantidad: Int = 1,
-    var mensaje: String = ""
+    var mensaje: String = "",
+    val userId: String = "",
+    val visto: Boolean = false,
+    val vistoAdmin: Boolean = false
+
+
 )
 
